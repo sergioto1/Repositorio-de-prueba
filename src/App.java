741c433182b748323deb074154a88bd4a84c9eb9
sergioto1 <sergio.trivino97@gmail.com>
2021-07-05 02:04:16 -0500

    Commit inicial

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre por favor");
+        String nombre = sc.nextLine();
+        sc.close();
+        System.out.println("Hola " + nombre + " !");
+    }
+}
